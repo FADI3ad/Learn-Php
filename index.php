ddc2dce072f5c2d2efbb32f480040471ed1e9d43
Foda <fadyemad687@gmail.com>
2025-04-01 13:05:15 +0200

    part 1 of str functions

--- index.php
+++ index.php
@@ -9,3 +9,11 @@
 // String Functions 
 
 echo strlen("Hello World");
+echo "<br>";
+echo lcfirst("Elzero Web School") . "<br>";
+echo ucfirst("elzero web school") . "<br>";
+echo strtolower("ELZERO WEB SCHOOL") . "<br>";
+echo strtoupper("elzero web school") . "<br>";
+echo ucwords("elzero web school") . "<br>";
+echo ucwords("elzero web|school", "|") . "<br>";
+echo str_repeat("Elzero", 3);
